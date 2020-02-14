286797b34a601a45884e7de49e2abccdd7af8be1
dib1987 <d.mondal87@gmail.com>
2020-02-14 22:16:06 +0530

    changes

---
+++ src/inrroduction1/demo2.java
@@ -0,0 +1,18 @@
+package inrroduction1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\work\\chromedrive.exe");
+		
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://www.google.com");
+		
+
+	}
+
+}
